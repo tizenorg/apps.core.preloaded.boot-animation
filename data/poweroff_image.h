3de9fc8ee2b172556f161e9d44073676e435b70a
junkyu.han <junkyu.han@samsung.com>
2015-05-30 17:21:35 +0900

    2.4 code merge
    Change-Id: I9107e699741027ca2687c6d4607e2ce36fa21f95

--- data/poweroff_image.h
+++ data/poweroff_image.h
@@ -1,68 +1,64 @@
 /*
-  * Copyright 2012  Samsung Electronics Co., Ltd
-  * 
-  * Licensed under the Flora License, Version 1.1 (the License);
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  * 
-  *     http://floralicense.org/license/
-  * 
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an AS IS BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ *  boot-animation
+ *
+ * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Seungtaek Chung <seungtaek.chung@samsung.com>, Mi-Ju Lee <miju52.lee@samsung.com>, Xi Zhichan <zhichan.xi@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-
- image:"HD/power_off_720_1280/power_off_720_1280__00000.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00001.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00002.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00003.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00004.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00005.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00006.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00007.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00008.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00009.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00010.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00011.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00012.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00013.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00014.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00015.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00016.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00017.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00018.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00019.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00020.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00021.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00022.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00023.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00024.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00025.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00026.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00027.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00028.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00029.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00030.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00031.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00032.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00033.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00034.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00035.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00036.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00037.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00038.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00039.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00040.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00041.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00042.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00043.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00044.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00045.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00046.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00047.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00048.png" LOSSY 85;
- image:"HD/power_off_720_1280/power_off_720_1280__00049.png" LOSSY 85;
-
+ image:"480X800_Power_off/Tizen_power_off_01.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_02.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_03.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_04.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_05.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_06.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_07.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_08.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_09.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_10.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_11.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_12.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_13.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_14.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_15.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_16.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_17.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_18.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_19.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_20.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_21.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_22.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_23.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_24.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_25.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_26.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_27.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_28.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_29.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_30.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_31.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_32.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_33.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_34.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_35.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_36.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_37.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_38.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_39.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_40.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_41.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_42.png" LOSSY 85;
+ image:"480X800_Power_off/Tizen_power_off_43.png" LOSSY 85;
--- src/animation.c
+++ src/animation.c
@@ -1,19 +1,23 @@
 /*
-  * Copyright 2012  Samsung Electronics Co., Ltd
-  * 
-  * Licensed under the Flora License, Version 1.1 (the License);
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  * 
-  *     http://floralicense.org/license/
-  * 
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an AS IS BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
-
+ *  boot-animation
+ *
+ * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Seungtaek Chung <seungtaek.chung@samsung.com>, Mi-Ju Lee <miju52.lee@samsung.com>, Xi Zhichan <zhichan.xi@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,17 +26,24 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 #include <unistd.h>
+#include <dirent.h>
 
-#include <utilX.h>
+//#include <utilX.h>
 #include <vconf.h>
 
 #include <Elementary.h>
-#include <Ecore_X.h>
-#include <Ecore_X_Atoms.h>
+//#include <Ecore_X.h>
+//#include <Ecore_X_Atoms.h>
 
-#include <boot-animation.h>
+#include <boot.h>
 #include <animation.h>
+
+#include "log.h"
+
+#define OVER_COUNT 19
 
 static struct animation {
 	Ecore_X_Window focus_win;
@@ -50,23 +61,108 @@ static struct animation {
 } s_animation = {
 .txt = NULL,};
 
+#define CHECK_Xorg "/tmp/.wm_ready"
+#define CHECK_LCD "/sys/class/lcd"
+
+static int run_child(const char *argv[])
+{
+	pid_t pid;
+	struct sigaction act, oldact;
+	int i, r = 0, status;
+
+	if (!argv)
+		return -EINVAL;
+
+	/* Use default signal handler */
+	act.sa_handler = SIG_DFL;
+	act.sa_sigaction = NULL;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+
+	if (sigaction(SIGCHLD, &act, &oldact) < 0)
+		return -errno;
+
+	pid = fork();
+	if (pid < 0) {
+		r = -errno;
+	} else if (pid == 0) {
+		for (i = 0; i < _NSIG; ++i)
+			signal(i, SIG_DFL);
+		r = execv(argv[0], (char **)argv);
+		if (r < 0)
+			exit(EXIT_FAILURE);
+	} else {
+		/* wait for child */
+		if (waitpid(pid, &status, 0) != -1)
+			/* terminated normally */
+			if (WIFEXITED(status))
+				return WEXITSTATUS(status);
+	}
+
+	if (sigaction(SIGCHLD, &oldact, NULL) < 0)
+		_E("Failed to restore sigaction");
+	return r;
+}
+
+static int check_window_validity(void)
+{
+	_D("Check window validity");
+	DIR *dp;
+	struct dirent *dentry;
+	int ret = -1;
+	dp = opendir(CHECK_LCD);
+	_D("Check window validity");
+	if (!dp) {
+		_E("Failed to get lcd node");
+		return -1;
+	}
+	while ((dentry = readdir(dp)) != NULL) {
+	_D("Check window validity");
+		if ((!strcmp(dentry->d_name, ".")) || (!strcmp(dentry->d_name, ".."))) {
+			continue;
+		}
+		else {
+	_D("Check window validity");
+			ret = 0;
+			break;
+		}
+	}
+	_D("Check window validity");
+	closedir(dp);
+	return ret;
+}
+
 static void win_del(void *data, Evas_Object * obj, void *event_info)
 {
-	fprintf(stderr, "Window delete event received\n");
+	_D("Window delete event received");
 	elm_exit();
+}
+
+static Eina_Bool _end_cb(void *data)
+{
+	static const char *argv[5] = {
+		"/usr/bin/xset",
+		"dpms", "force", "off", NULL,
+	};
+	_D("_end_cb is invoked");
+	if (vconf_set_int(VCONFKEY_BOOT_ANIMATION_FINISHED, 1) != 0)
+		_E("Failed to set finished set");
+	if (s_animation.state == TYPE_OFF || s_animation.state == TYPE_OFF_WITH_MSG)
+		run_child(argv);
+	_D("_end_cb is invoked");
+	elm_exit();
+	_D("_end_cb is invoked");
+	return ECORE_CALLBACK_CANCEL;
 }
 
 static void _edje_cb(void *d, Evas_Object * obj, const char *e, const char *s)
 {
-	fprintf(stderr, "edje calback is invoked\n");
+	_D("edje callback is invoked");
 
-	if (vconf_set_int("memory/boot-animation/finished", 1) != 0) {
-		fprintf(stderr, "Failed to set finished set\n");
-	}
-
-	if (s_animation.state == TYPE_OFF || s_animation.state == TYPE_ON) {
-		elm_exit();
-	} else if (s_animation.state == TYPE_OFF_WITH_MSG) {
+	if (s_animation.state == TYPE_OFF || s_animation.state == TYPE_OFF_WITH_MSG) {
+		_D("TYPE OFF");
+		if (vconf_set_int(VCONFKEY_BOOT_ANIMATION_FINISHED, 1) != 0)
+			_E("Failed to set finished set");
 		if (s_animation.txt) {
 			Evas_Coord w;
 			Evas_Coord h;
@@ -90,42 +186,66 @@ static void _edje_cb(void *d, Evas_Object * obj, const char *e, const char *s)
 					 (s_animation.h - h) >> 1);
 			evas_object_show(s_animation.txt);
 		}
+		ecore_timer_add(1, _end_cb, NULL);
+	} else
+		_D("TYPE_ON");
+		_end_cb(NULL);
+}
+
+#define DEFAULT_W 480
+static void layout_file_set(int state)
+{
+	_D("Layout file set according to resolution");
+	_D("Screen Width: %d, DEFAULT_WIDTH: %d", s_animation.w, DEFAULT_W);
+
+	if (s_animation.w < DEFAULT_W) {
+		if (state == TYPE_ON) {
+			_D("Set file: %s", WEARABLE_EDJ_ON);
+			elm_layout_file_set(s_animation.layout, WEARABLE_EDJ_ON, GRP_ON);
+		} else {
+			_D("Set file: %s", WEARABLE_EDJ_OFF);
+			elm_layout_file_set(s_animation.layout, WEARABLE_EDJ_OFF, GRP_OFF);
+		}
+	} else if (s_animation.w > DEFAULT_W) {
+		if (state == TYPE_ON) {
+			_D("Set file: %s", MOBILE_EDJ_ON);
+			elm_layout_file_set(s_animation.layout, MOBILE_EDJ_ON, GRP_ON);
+		} else {
+			_D("Set file: %s", MOBILE_EDJ_OFF);
+			elm_layout_file_set(s_animation.layout, MOBILE_EDJ_OFF, GRP_OFF);
+		}
+	} else {
+		if (state == TYPE_ON) {
+			_D("Set file: %s", DEFAULT_EDJ_ON);
+			elm_layout_file_set(s_animation.layout, DEFAULT_EDJ_ON, GRP_ON);
+		} else {
+			_D("Set file: %s", DEFAULT_EDJ_OFF);
+			elm_layout_file_set(s_animation.layout, DEFAULT_EDJ_OFF, GRP_OFF);
+		}
 	}
 }
 
-static
-int init_layout(const char *msg)
+static int init_layout(const char *msg)
 {
 	s_animation.layout = elm_layout_add(s_animation.win);
 	if (!s_animation.layout) {
-		fprintf(stderr, "Failed to create layout\n");
+		_E("Failed to create layout");
 		return EXIT_FAILURE;
 	}
 
-	if (s_animation.state == TYPE_ON) {
-		elm_layout_file_set(s_animation.layout, EDJ_ON, GRP_ON);
-	} else {
-		elm_layout_file_set(s_animation.layout, EDJ_OFF, GRP_OFF);
-	}
+	layout_file_set(s_animation.state);
 
 	evas_object_size_hint_weight_set(s_animation.layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	printf("[Boot-ani] Start animation: %d mode\n", s_animation.state);
+	_D("[Boot-ani] Start animation: %d mode", s_animation.state);
 	elm_win_resize_object_add(s_animation.win, s_animation.layout);
-	edje_object_signal_callback_add(elm_layout_edje_get(s_animation.layout),
-					"end", "animation", _edje_cb, NULL);
+	edje_object_signal_callback_add(elm_layout_edje_get(s_animation.layout), "end", "animation", _edje_cb, NULL);
 	evas_object_show(s_animation.layout);
-
-	Ecore_X_Window xwin;
-	xwin = elm_win_xwindow_get(s_animation.win);
-	ecore_x_icccm_hints_set(xwin, 0, 0, 0, 0, 0, 0, 0);
-
 
 	if (msg) {
 		if (!s_animation.txt) {
-			s_animation.txt =
-			    evas_object_text_add(s_animation.evas);
+			s_animation.txt = evas_object_text_add(s_animation.evas);
 			if (!s_animation.txt) {
-				fprintf(stderr, "Failed to add text\n");
+				_E("Failed to add text");
 				evas_object_del(s_animation.layout);
 				return EXIT_FAILURE;
 			}
@@ -135,15 +255,10 @@ int init_layout(const char *msg)
 		evas_object_hide(s_animation.txt);
 	}
 
-	if (vconf_set_int("memory/boot-animation/finished", 0) != 0) {
-		fprintf(stderr, "Failed to set finished value to 0\n");
-	}
-
 	return EXIT_SUCCESS;
 }
 
-static
-void fini_layout(void)
+static void fini_layout(void)
 {
 	if (s_animation.layout) {
 		evas_object_del(s_animation.layout);
@@ -155,82 +270,115 @@ void fini_layout(void)
 	}
 }
 
-void restart_animation(int state, const char *msg)
+static int create_window(void)
 {
-	fini_layout();
+	_D("Create Window");
 
-	s_animation.state = state;
+	int x, y = 0;
 
-	init_layout(msg);
-
-	evas_object_show(s_animation.win);
-}
-
-static
-int create_window(void)
-{
-	s_animation.win = elm_win_add(NULL, "", ELM_WIN_BASIC);
+	s_animation.win = elm_win_add(NULL, "", ELM_WIN_NOTIFICATION);
 	if (!s_animation.win) {
-		fprintf(stderr, "Failed to create a new window\n");
+		_E("Failed to create a new window");
 		return EXIT_FAILURE;
 	}
-	evas_object_smart_callback_add(s_animation.win, "delete-request",
-				       win_del, NULL);
-	evas_object_resize(s_animation.win, s_animation.w, s_animation.h);
+	if (s_animation.state == TYPE_OFF || s_animation.state == TYPE_OFF_WITH_MSG) {
+		_D("We are turning off the Tizen");
+		elm_win_alpha_set(s_animation.win, EINA_TRUE);
+	}
+	evas_object_smart_callback_add(s_animation.win, "delete-request", win_del, NULL);
 
 	s_animation.evas = evas_object_evas_get(s_animation.win);
 	if (!s_animation.evas) {
 		evas_object_del(s_animation.win);
-		fprintf(stderr, "Failed to get the evas object\n");
+		_E("Failed to get the evas object");
 		return EXIT_FAILURE;
 	}
+	elm_win_screen_size_get(s_animation.win, &x, &y, &s_animation.w, &s_animation.h);
+	_D("Window size is x: %d, y: %d, w: %d, h: %d", x, y, s_animation.w, s_animation.h);
 	elm_win_borderless_set(s_animation.win, 1);
+	elm_win_indicator_mode_set(s_animation.win, ELM_WIN_INDICATOR_HIDE);
 	evas_object_move(s_animation.win, 0, 0);
 
 	if (s_animation.w > s_animation.h) {
 		int t;
 		elm_win_rotation_with_resize_set(s_animation.win, 90);
+		/* Update geometry infomration */
 		t = s_animation.w;
 		s_animation.w = s_animation.h;
 		s_animation.h = t;
 	}
+
 	evas_object_show(s_animation.win);
 
 	s_animation.ee = ecore_evas_ecore_evas_get(s_animation.evas);
 	if (!s_animation.ee) {
 		evas_object_del(s_animation.win);
-		fprintf(stderr, "Failed to get the ecore evas object\n");
+		_E("Failed to get the ecore evas object");
 		return EXIT_FAILURE;
 	}
 
-	s_animation.xwin = ecore_evas_window_get(s_animation.ee);
-	ecore_x_netwm_window_type_set(s_animation.xwin,
-				      ECORE_X_WINDOW_TYPE_NOTIFICATION);
-	utilx_set_system_notification_level(ecore_x_display_get(),
-					    s_animation.xwin,
-					    UTILX_NOTIFICATION_LEVEL_HIGH);
+	//s_animation.xwin = ecore_evas_window_get(s_animation.ee);
+	//utilx_set_system_notification_level(ecore_x_display_get(), s_animation.xwin, UTILX_NOTIFICATION_LEVEL_HIGH);
 
 	return EXIT_SUCCESS;
 }
 
+static Eina_Bool _count_for_xorg_timer_cb(void *data)
+{
+	int ret = 0;
+	static int count = 0;
+
+	ret = access(CHECK_Xorg, F_OK);
+	if (ret == 0) {
+		if (init_layout(data) == EXIT_FAILURE) {
+			_E("Failed to init the layout object");
+			if (data) {
+				evas_object_del(s_animation.txt);
+			}
+			evas_object_del(s_animation.win);
+		}
+		count = 0;
+		return ECORE_CALLBACK_CANCEL;
+	}
+	if (count > OVER_COUNT) {
+		_E("Getting Xorg is delaying, Something is wrong");
+		if (init_layout(data) == EXIT_FAILURE) {
+			_E("Failed to init the layout object");
+			if (data) {
+				evas_object_del(s_animation.txt);
+			}
+			evas_object_del(s_animation.win);
+		}
+		count = 0;
+		return ECORE_CALLBACK_CANCEL;
+	}
+	count ++;
+
+	return ECORE_CALLBACK_RENEW;
+}
+
 int init_animation(int state, const char *msg)
 {
-	s_animation.focus_win = ecore_x_window_focus_get();
-	s_animation.root_win = ecore_x_window_root_get(s_animation.focus_win);
+	_D("Init animation");
+
+	Ecore_Timer *timer = NULL;
+
+	//if (check_window_validity()) {
+	//	_E("Failed to access LCD");
+	//	return EXIT_FAILURE;
+	//}
+
 	s_animation.state = state;
-	ecore_x_window_size_get(s_animation.root_win, &s_animation.w,
-				&s_animation.h);
 
 	if (create_window() == EXIT_FAILURE) {
-		fprintf(stderr, "Failed to create a new window\n");
+		_E("Failed to create a new window");
 		return EXIT_FAILURE;
 	}
 
-	if (init_layout(msg) == EXIT_FAILURE) {
-		fprintf(stderr, "Failed to init the layout object\n");
-		if (msg)
-			evas_object_del(s_animation.txt);
-		evas_object_del(s_animation.win);
+	/* waiting initialzing Xorg process during 2 seconds */
+	timer = ecore_timer_add(0.1f, _count_for_xorg_timer_cb, msg);
+	if (!timer) {
+		_E("Critical Error");
 		return EXIT_FAILURE;
 	}
 
